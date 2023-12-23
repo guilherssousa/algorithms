8151c207a04cd67198331e4c1c0b9d1a8295a4c8
Guilherme Sousa <guilherssousa@gmail.com>
2023-12-23 10:17:40 -0300

    Implement tests for binary search

--- binary-search.py
+++ binary-search.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def binary_search(arr, target):
     left = 0
     right = len(arr) - 1
@@ -20,3 +23,19 @@ sample = [3, 7, 9, 11, 12, 15, 16, 18, 20]
 index = binary_search(sample, 7)
 
 print(index)
+
+
+class SearchTest(unittest.TestCase):
+    def test_binary_search(self):
+        sample_array = [3, 7, 9, 11, 12, 15, 16, 18, 20]
+        target = 7
+        self.assertEqual(binary_search(sample_array, target), 1)
+
+    def test_binary_search_will_return_negative_when_number_not_found(self):
+        sample_array = [3, 7, 9, 11, 12, 15, 16, 18, 20]
+        target = 8
+        self.assertEqual(binary_search(sample_array, target), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
