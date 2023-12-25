ba16b334ebad8b3a70d030ba4a397766e9bba7bf
Guilherme Sousa <guilherssousa@gmail.com>
2023-12-25 00:45:06 -0300

    add bash

--- bash/bubble_sort.sh
+++ bash/bubble_sort.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 bubble_sort() {
-  for i in $1; do
-    for j in $(($1-$i-1)); do
-      if [ $j -gt $j+1 ]; then
-      swap $j $j+1
-      fi
+  local -n arr=$1
+  local len=${#arr[@]}
+  local i j temp
+  ``
+  for ((i=0; i<$len; i++)); do
+    for ((j=0; j<$len-i-1; j++)); do
+        if [[ ${arr[$j]} -gt ${arr[$j+1]} ]]; then
+            temp=${arr[$j]}
+            arr[$j]=${arr[$j+1]}
+            arr[$j+1]=$temp
+        fi
     done
   done
+
+  echo "${arr[@]}"
 }
 
 sample=(12 5 3 34 122 0 8 2 9 1 3 2 5 7 120)
 echo "Unsorted: ${sample[@]}"
-echo "Sorted: $(bubble_sort ${sample[@]})"
+sorted=($(bubble_sort sample))
+echo "Sorted: ${sorted[@]}"
